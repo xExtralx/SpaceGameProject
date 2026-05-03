96daa59448724393ab47dd10cd60ef25b424ea9d
Ubuntu <ubuntu@vps-c4c7b8d5.vps.ovh.net>
2026-05-03 12:04:14 +0000

    Auto build 2026-05-03 12:04:14

--- build-windows/CPM_modules/FindFastSIMD.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/ubuntu/Games/SpaceGameProject/external/FastNoise2/cmake/CPM.cmake")
-CPMAddPackage("NAME;FastSIMD;GITHUB_REPOSITORY;Auburn/FastSIMD;GIT_TAG;16450dae9528727e500e7254f635a671f9c7ee2d")
-set(FastSIMD_FOUND TRUE)
--- build-windows/external/FastNoise2/src/fastsimd/FastSIMD_FastNoise/FastSIMD_FastNoise_SSE41.cpp
+++
@@ -1,9 +0,0 @@
-#define FASTSIMD_MAX_FEATURE_SET SSE41
-#include <FastSIMD/Utility/ArchDetect.h>
-
-#if 1
-#include <FastSIMD/FastSIMD_FastNoise_config.h>
-
-#include "/home/ubuntu/Games/SpaceGameProject/build-windows/_deps/fastsimd-src/dispatch/impl/DispatchClassImpl.h"
-#include "/home/ubuntu/Games/SpaceGameProject/external/FastNoise2/src/FastNoise/FastSIMD_Build.inl"
-#endif
