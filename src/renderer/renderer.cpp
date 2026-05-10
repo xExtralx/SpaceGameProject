b73bae7fa07f8f33faa274a89b5b703dd44825fd
Ubuntu <ubuntu@vps-c4c7b8d5.vps.ovh.net>
2026-05-10 15:23:51 +0000

    Auto build 2026-05-10 15:23:41

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -211,7 +211,7 @@ void Renderer::initTileBuffer() {
 void Renderer::clear() {
     glBindFramebuffer(GL_FRAMEBUFFER, pixelFBO);
     glViewport(0, 0, RENDER_WIDTH, RENDER_HEIGHT);
-    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     tileVertices.clear();
